bece18c298954c994ef270e3708d56656cc30bbb
T3kla <manuillo94@gmail.com>
2021-10-26 13:46:02 +0200

    arrows

--- exercise1handout/exercise1.h
+++ exercise1handout/exercise1.h
@@ -9,156 +9,188 @@
 #include "lineshapes.h"
 #include "maths_funcs.h"
 
-struct Exercise1 {
+struct Exercise1
+{
+    GLFWwindow *window = NULL;
 
-  GLFWwindow *window = NULL;
+    vec3 ambientColor = vec3(0.7f, 0.7f, 0.75f);
+    GLuint lines_shader_index;
 
-  vec3 ambientColor = vec3(0.7f, 0.7f, 0.75f);
-  GLuint lines_shader_index;
+    Camera camera;
+    mat4 cameraMatrix;
+    vec3 cameraPosition;
 
-  Camera camera;
-  mat4 cameraMatrix;
-  vec3 cameraPosition;
+    Lines cubeLines;
+    mat4 cubeMatrix;
+    vec3 cubePosition;
+    float cubeSpeed;
 
-  Lines cubeLines;
-  mat4 cubeMatrix;
-  vec3 cubePosition;
+    Lines referenceFrameLines;
+    mat4 referenceFrameMatrix;
 
-  Lines referenceFrameLines;
-  mat4 referenceFrameMatrix;
+    void init(int width, int height)
+    {
 
-  void init(int width, int height) {
+        // init
+        // restart_gl_log();
+        startGlContext(&window, width, height);
 
-    // init
-    // restart_gl_log();
-    startGlContext(&window, width, height);
+        lines_shader_index = create_programme_from_files("lines_vs.glsl", "lines_fs.glsl");
 
-    lines_shader_index =
-        create_programme_from_files("lines_vs.glsl", "lines_fs.glsl");
+        vec3 whiteColor = vec3(1.0f, 1.0f, 1.0f);
 
-    vec3 whiteColor = vec3(1.0f, 1.0f, 1.0f);
+        vec3 cubeVertexPositions[] = {
+            {-1, -1, 1}, {1, -1, 1}, {1, 1, 1}, {-1, 1, 1}, {-1, -1, -1}, {1, -1, -1}, {1, 1, -1}, {-1, 1, -1},
+        };
 
-    vec3 cubeVertexPositions[] = {
-        {-1, -1, 1},  {1, -1, 1},  {1, 1, 1},  {-1, 1, 1},
-        {-1, -1, -1}, {1, -1, -1}, {1, 1, -1}, {-1, 1, -1},
-    };
+        vec3 cubeVertexColors[] = {
+            whiteColor, whiteColor, whiteColor, whiteColor, whiteColor, whiteColor, whiteColor, whiteColor,
+        };
 
-    vec3 cubeVertexColors[] = {
-        whiteColor, whiteColor, whiteColor, whiteColor,
-        whiteColor, whiteColor, whiteColor, whiteColor,
-    };
+        GLuint cubeLineIndices[] = {
+            0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4, 0, 4, 1, 5, 2, 6, 3, 7,
+        };
 
-    GLuint cubeLineIndices[] = {
-        0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4,
+        cubeLines.add(&cubeVertexPositions[0].v[0], &cubeVertexColors[0].v[0], 8, &cubeLineIndices[0], 12 * 2);
+        cubeLines.load_to_gpu();
+        cubeMatrix = identity_mat4();
+        cubePosition = vec3(2, 0, 0);
+        cubeSpeed = 3.0f;
 
-        0, 4, 1, 5, 2, 6, 3, 7,
+        Shapes::addArrow(referenceFrameLines, vec3(0, 0, 0), vec3(1, 0, 0), vec3(1, 0, 0));
+        Shapes::addArrow(referenceFrameLines, vec3(0, 0, 0), vec3(0, 1, 0), vec3(0, 1, 0));
+        Shapes::addArrow(referenceFrameLines, vec3(0, 0, 0), vec3(0, 0, 1), vec3(0, 0, 1));
+        // ---------------------------------------------------------------
+        Shapes::addArrow(referenceFrameLines, vec3(1, 0, 1), vec3(1, 1, 1), vec3(0, 0, 0));
+        Shapes::addArrow(referenceFrameLines, vec3(1, 0, 1), vec3(1, -1, 1), vec3(0, 0, 0));
+        Shapes::addArrow(referenceFrameLines, vec3(3, 0, 1), vec3(3, 1, 1), vec3(0, 0, 0));
+        Shapes::addArrow(referenceFrameLines, vec3(3, 0, 1), vec3(3, -1, 1), vec3(0, 0, 0));
+        // ---------------------------------------------------------------
 
-    };
+        referenceFrameLines.load_to_gpu();
+        referenceFrameLines.get_shader_locations(lines_shader_index);
 
-    cubeLines.add(&cubeVertexPositions[0].v[0], &cubeVertexColors[0].v[0], 8,
-                  &cubeLineIndices[0], 12 * 2);
-    cubeLines.load_to_gpu();
-    cubeMatrix = identity_mat4();
-    cubePosition = vec3(0, 0, 0);
+        referenceFrameMatrix = identity_mat4();
 
-    Shapes::addArrow(referenceFrameLines, vec3(0, 0, 0), vec3(1, 0, 0),
-                     vec3(1, 0, 0));
-    Shapes::addArrow(referenceFrameLines, vec3(0, 0, 0), vec3(0, 1, 0),
-                     vec3(0, 1, 0));
-    Shapes::addArrow(referenceFrameLines, vec3(0, 0, 0), vec3(0, 0, 1),
-                     vec3(0, 0, 1));
+        // camera
+        camera.near = 0.1f;
+        camera.far = 1000.0f;
+        camera.fov = 67.0f * ONE_DEG_IN_RAD;                    // convert degrees to radians
+        camera.aspect = (float)g_gl_width / (float)g_gl_height; // aspect ratio
 
-    referenceFrameLines.load_to_gpu();
-    referenceFrameLines.get_shader_locations(lines_shader_index);
+        camera.updateProjection();
+        camera.speed = 5.0f;
+        camera.yaw_speed = 120.f;
 
-    referenceFrameMatrix = identity_mat4();
+        cameraMatrix = identity_mat4();
+        cameraPosition = vec3(1, 1, 7);
 
-    // camera
-    camera.near = 0.1f;
-    camera.far = 1000.0f;
-    camera.fov = 67.0f * ONE_DEG_IN_RAD; // convert degrees to radians
-    camera.aspect = (float)g_gl_width / (float)g_gl_height; // aspect ratio
-
-    camera.updateProjection();
-    camera.speed = 5.0f;
-    camera.yaw_speed = 120.f;
-
-    cameraMatrix = identity_mat4();
-    cameraPosition = vec3(0, 0, 7);
-
-    // tell GL to only draw onto a pixel if the shape is closer to the viewer
-    glEnable(GL_DEPTH_TEST); // enable depth-testing
-    glDepthFunc(
-        GL_LESS); // depth-testing interprets a smaller value as "closer"
-    glEnable(GL_CULL_FACE); // cull face
-    glCullFace(GL_BACK);    // cull back face
-    glFrontFace(GL_CCW);    // GL_CCW for counter clock-wise
-  }
-
-  void update() {
-    static float previous_seconds = static_cast<float>(glfwGetTime());
-    float current_seconds = static_cast<float>(glfwGetTime());
-    float elapsed_seconds = current_seconds - previous_seconds;
-    previous_seconds = current_seconds;
-
-    _update_fps_counter(window);
-    // wipe the drawing surface clear
-    glClearColor(ambientColor.v[0], ambientColor.v[1], ambientColor.v[2], 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glViewport(0, 0, g_gl_width, g_gl_height);
-
-    glfwPollEvents();
-    if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE)) {
-      glfwSetWindowShouldClose(window, 1);
+        // tell GL to only draw onto a pixel if the shape is closer to the viewer
+        glEnable(GL_DEPTH_TEST); // enable depth-testing
+        glDepthFunc(GL_LESS);    // depth-testing interprets a smaller value as "closer"
+        glEnable(GL_CULL_FACE);  // cull face
+        glCullFace(GL_BACK);     // cull back face
+        glFrontFace(GL_CCW);     // GL_CCW for counter clock-wise
     }
 
-    // ---------------------------------------------------------------
+    void update()
+    {
+        static float previous_seconds = static_cast<float>(glfwGetTime());
+        float current_seconds = static_cast<float>(glfwGetTime());
+        float elapsed_seconds = current_seconds - previous_seconds;
+        previous_seconds = current_seconds;
 
-    if (glfwGetKey(window, GLFW_KEY_DOWN)) {
-      cameraPosition += vec3(0, -1, 0) * elapsed_seconds;
+        _update_fps_counter(window);
+        // wipe the drawing surface clear
+        glClearColor(ambientColor.v[0], ambientColor.v[1], ambientColor.v[2], 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glViewport(0, 0, g_gl_width, g_gl_height);
+
+        glfwPollEvents();
+        if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE))
+        {
+            glfwSetWindowShouldClose(window, 1);
+        }
+
+        // ---------------------------------------------------------------
+
+        if (glfwGetKey(window, GLFW_KEY_UP))
+        {
+            cubePosition = vec3(0, 1, 0) * cubeSpeed * elapsed_seconds;
+        }
+        if (glfwGetKey(window, GLFW_KEY_DOWN))
+        {
+            cubePosition = vec3(0, -1, 0) * cubeSpeed * elapsed_seconds;
+        }
+        if (glfwGetKey(window, GLFW_KEY_LEFT))
+        {
+            cubePosition = vec3(-1, 0, 0) * cubeSpeed * elapsed_seconds;
+        }
+        if (glfwGetKey(window, GLFW_KEY_RIGHT))
+        {
+            cubePosition = vec3(1, 0, 0) * cubeSpeed * elapsed_seconds;
+        }
+
+        if (glfwGetKey(window, GLFW_KEY_W))
+        {
+            cameraPosition += vec3(0, 1, 0) * elapsed_seconds;
+        }
+        if (glfwGetKey(window, GLFW_KEY_S))
+        {
+            cameraPosition += vec3(0, -1, 0) * elapsed_seconds;
+        }
+        if (glfwGetKey(window, GLFW_KEY_A))
+        {
+            cameraPosition += vec3(-1, 0, 0) * elapsed_seconds;
+        }
+        if (glfwGetKey(window, GLFW_KEY_D))
+        {
+            cameraPosition += vec3(1, 0, 0) * elapsed_seconds;
+        }
+
+        versor q;
+        vec3 p, s;
+        cubeMatrix.decompose(q, p, s);
+
+        // rotate cube from pivot, not orig
+        cubeMatrix = translate(cubeMatrix, p * -1);                    // place in 0,0,0
+        cubeMatrix = rotate_y_deg(cubeMatrix, 50.f * elapsed_seconds); // rotate cube
+        cubeMatrix = translate(cubeMatrix, cubePosition + p);          // place wherever it was before
+
+        cameraMatrix = translate(identity_mat4(), cameraPosition * -1.f);
+
+        // because i'm using 'translate(cubeMatrix' to keep rotation
+        cubePosition = vec3(0, 0, 0);
+
+        // ---------------------------------------------------------------
+
+        glUseProgram(lines_shader_index);
+
+        camera.get_shader_locations(lines_shader_index);
+        camera.set_shader_data(lines_shader_index, cameraMatrix);
+
+        referenceFrameLines.get_shader_locations(lines_shader_index);
+        referenceFrameLines.set_shader_data(lines_shader_index, referenceFrameMatrix);
+        referenceFrameLines.render(lines_shader_index);
+
+        cubeLines.get_shader_locations(lines_shader_index);
+        cubeLines.set_shader_data(lines_shader_index, cubeMatrix);
+        cubeLines.render(lines_shader_index);
+
+        glUseProgram(0);
+
+        // put the stuff we've been drawing onto the display
+        glfwSwapBuffers(window);
     }
-    if (glfwGetKey(window, GLFW_KEY_UP)) {
-      cameraPosition += vec3(0, 1, 0) * elapsed_seconds;
-    }
-    if (glfwGetKey(window, GLFW_KEY_LEFT)) {
-      cameraPosition += vec3(-1, 0, 0) * elapsed_seconds;
-    }
-    if (glfwGetKey(window, GLFW_KEY_RIGHT)) {
-      cameraPosition += vec3(1, 0, 0) * elapsed_seconds;
+
+    bool isLoopGo()
+    {
+        return !glfwWindowShouldClose(window);
     }
 
-    cubeMatrix = rotate_y_deg(cubeMatrix, 50.f * elapsed_seconds);
-
-    cameraMatrix = translate(identity_mat4(), cameraPosition * -1.f);
-
-    cubeMatrix = translate(cubeMatrix, cubePosition);
-
-    // ---------------------------------------------------------------
-
-    glUseProgram(lines_shader_index);
-
-    camera.get_shader_locations(lines_shader_index);
-    camera.set_shader_data(lines_shader_index, cameraMatrix);
-
-    referenceFrameLines.get_shader_locations(lines_shader_index);
-    referenceFrameLines.set_shader_data(lines_shader_index,
-                                        referenceFrameMatrix);
-    referenceFrameLines.render(lines_shader_index);
-
-    cubeLines.get_shader_locations(lines_shader_index);
-    cubeLines.set_shader_data(lines_shader_index, cubeMatrix);
-    cubeLines.render(lines_shader_index);
-
-    glUseProgram(0);
-
-    // put the stuff we've been drawing onto the display
-    glfwSwapBuffers(window);
-  }
-
-  bool isLoopGo() { return !glfwWindowShouldClose(window); }
-
-  void terminate() {
-    // close GL context and any other GLFW resources
-    glfwTerminate();
-  }
+    void terminate()
+    {
+        // close GL context and any other GLFW resources
+        glfwTerminate();
+    }
 };
--- exercise1handout/lineshapes.cpp
+++ exercise1handout/lineshapes.cpp
@@ -1,149 +1,162 @@
 #include "lineshapes.h"
 
-void Lines::add(GLfloat *vertex_data, GLfloat *color_data, size_t vertexCount,
-                GLuint *indices_data, size_t indexCount) {
-  size_t point_size = points.size();
-  size_t index_size = indices.size();
+void Lines::add(GLfloat *vertex_data, GLfloat *color_data, size_t vertexCount, GLuint *indices_data, size_t indexCount)
+{
+    size_t point_size = points.size();
+    size_t index_size = indices.size();
 
-  points.resize(point_size + vertexCount);
-  colors.resize(point_size + vertexCount);
-  indices.resize(index_size + indexCount);
+    points.resize(point_size + vertexCount);
+    colors.resize(point_size + vertexCount);
+    indices.resize(index_size + indexCount);
 
-  for (size_t i = 0; i < vertexCount; ++i) {
-    points[point_size + i].v[0] = vertex_data[i * 3];
-    points[point_size + i].v[1] = vertex_data[i * 3 + 1];
-    points[point_size + i].v[2] = vertex_data[i * 3 + 2];
-  }
-  for (size_t i = 0; i < vertexCount; ++i) {
-    colors[point_size + i].v[0] = color_data[i * 3];
-    colors[point_size + i].v[1] = color_data[i * 3 + 1];
-    colors[point_size + i].v[2] = color_data[i * 3 + 2];
-  }
-  for (size_t i = 0; i < indexCount; ++i) {
-    indices[index_size + i] = point_size + indices_data[i];
-  }
-}
-
-void Lines::clear() {
-  points.clear();
-  colors.clear();
-  indices.clear();
-}
-
-void Lines::load_to_gpu() {
-
-  size_t vertex_count = points.size();
-  size_t index_count = indices.size();
-
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-
-  size_t attribIx = 0;
-
-  if (vertex_count) {
-    glGenBuffers(1, &points_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-    glBufferData(GL_ARRAY_BUFFER, 3 * vertex_count * sizeof(GLfloat),
-                 &points[0].v[0], GL_STATIC_DRAW);
-    glEnableVertexAttribArray(attribIx);
-    glVertexAttribPointer(attribIx, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-  }
-  ++attribIx;
-
-  if (vertex_count) {
-    glGenBuffers(1, &colors_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glBufferData(GL_ARRAY_BUFFER, 3 * vertex_count * sizeof(GLfloat),
-                 &colors[0].v[0], GL_STATIC_DRAW);
-    glEnableVertexAttribArray(attribIx);
-    glVertexAttribPointer(attribIx, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-  }
-
-  ++attribIx;
-
-  if (index_count) {
-    glGenBuffers(1, &index_vbo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_vbo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * index_count,
-                 &indices[0], GL_STATIC_DRAW);
-  }
-  glBindVertexArray(0);
-}
-
-void Lines::get_shader_locations(GLuint shader_programme) {
-  model_matrix_location = glGetUniformLocation(shader_programme, "model");
-}
-
-void Lines::set_shader_data(GLuint shader_programme,
-                            const mat4 &referenceFrameMatrix) {
-  glUniformMatrix4fv(model_matrix_location, 1, GL_FALSE,
-                     referenceFrameMatrix.m);
-}
-
-void Lines::render(GLuint shader_programme) {
-  size_t index_count = indices.size();
-  glBindVertexArray(vao);
-  glDrawElements(GL_LINES, index_count, GL_UNSIGNED_INT, 0);
-  glBindVertexArray(0);
-}
-
-void Shapes::addArrow(Lines &lines, const vec3 &from, const vec3 &to,
-                      const vec3 &color) {
-  vec3 one(1, 1, 1);
-
-  // a = producto vectorial de one y vector me da vector a 45º de la
-  // perpendicular
-
-  // invertir eje, pasar a unitario, multiplicar por length, proyectar en a
-
-  vec3 arrows[] = {
-      from,
-      to,
-  };
-
-  vec3 colors[] = {
-      color,
-      color,
-  };
-
-  unsigned int arrow_indices[] = {
-      0, 1, // draw line from arrow_vertices[0] to arrow_vertices[1]
-  };
-
-  lines.add(&arrows[0].v[0], &colors[0].v[0], 2, &arrow_indices[0], 2);
-}
-
-void Shapes::addGrid(Lines &lines, const vec3 &from, const vec3 &to,
-                     const vec3 &color, int divs) {
-  int steps = divs + 1;
-
-  vec3 size = to - from;
-  float x = size.v[0] / (steps - 1);
-  float y = size.v[2] / (steps - 1);
-
-  std::vector<vec3> vertices;
-  vertices.resize(steps * steps);
-  std::vector<vec3> colors;
-  colors.resize(steps * steps);
-
-  for (int i = 0; i < steps; ++i) {
-    for (int j = 0; j < steps; ++j) {
-      vertices[j + i * steps] = vec3(from.v[0] + j * x, 0.f, from.v[2] + i * y);
-      colors[j + i * steps] = color;
+    for (size_t i = 0; i < vertexCount; ++i)
+    {
+        points[point_size + i].v[0] = vertex_data[i * 3];
+        points[point_size + i].v[1] = vertex_data[i * 3 + 1];
+        points[point_size + i].v[2] = vertex_data[i * 3 + 2];
     }
-  }
+    for (size_t i = 0; i < vertexCount; ++i)
+    {
+        colors[point_size + i].v[0] = color_data[i * 3];
+        colors[point_size + i].v[1] = color_data[i * 3 + 1];
+        colors[point_size + i].v[2] = color_data[i * 3 + 2];
+    }
+    for (size_t i = 0; i < indexCount; ++i)
+    {
+        indices[index_size + i] = point_size + indices_data[i];
+    }
+}
 
-  std::vector<unsigned int> indices;
-  indices.resize(4 * steps);
-  for (int i = 0; i < steps; ++i) {
-    indices[i * 2] = i;
-    indices[i * 2 + 1] = i + steps * (steps - 1);
-  }
-  for (int i = 0; i < steps; ++i) {
-    indices[steps * 2 + i * 2] = i * steps;
-    indices[steps * 2 + i * 2 + 1] = i * steps + (steps - 1);
-  }
+void Lines::clear()
+{
+    points.clear();
+    colors.clear();
+    indices.clear();
+}
 
-  lines.add(&vertices[0].v[0], &colors[0].v[0], vertices.size(), &indices[0],
-            indices.size());
+void Lines::load_to_gpu()
+{
+
+    size_t vertex_count = points.size();
+    size_t index_count = indices.size();
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    size_t attribIx = 0;
+
+    if (vertex_count)
+    {
+        glGenBuffers(1, &points_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, 3 * vertex_count * sizeof(GLfloat), &points[0].v[0], GL_STATIC_DRAW);
+        glEnableVertexAttribArray(attribIx);
+        glVertexAttribPointer(attribIx, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    }
+    ++attribIx;
+
+    if (vertex_count)
+    {
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, 3 * vertex_count * sizeof(GLfloat), &colors[0].v[0], GL_STATIC_DRAW);
+        glEnableVertexAttribArray(attribIx);
+        glVertexAttribPointer(attribIx, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    }
+
+    ++attribIx;
+
+    if (index_count)
+    {
+        glGenBuffers(1, &index_vbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_vbo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * index_count, &indices[0], GL_STATIC_DRAW);
+    }
+    glBindVertexArray(0);
+}
+
+void Lines::get_shader_locations(GLuint shader_programme)
+{
+    model_matrix_location = glGetUniformLocation(shader_programme, "model");
+}
+
+void Lines::set_shader_data(GLuint shader_programme, const mat4 &referenceFrameMatrix)
+{
+    glUniformMatrix4fv(model_matrix_location, 1, GL_FALSE, referenceFrameMatrix.m);
+}
+
+void Lines::render(GLuint shader_programme)
+{
+    size_t index_count = indices.size();
+    glBindVertexArray(vao);
+    glDrawElements(GL_LINES, index_count, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+void Shapes::addArrow(Lines &lines, const vec3 &from, const vec3 &to, const vec3 &color)
+{
+    vec3 one(0.2354846f, 0.2168565f, 0.2516546f);
+    vec3 two(-0.2354846f, 0.2168565f, 0.2516546f);
+
+    auto sharpness = 0.1f; // range [0,1] : 0 = sharpest, 1 = flattest
+    auto length = 0.15f;
+
+    auto dir = to - from;
+    auto inv = from - to;
+
+    auto pv1 = normalise(normalise(cross(one, dir)) * sharpness + normalise(inv) * 0.5f) * length + to;
+    auto pv2 = normalise(normalise(cross(one, inv)) * sharpness + normalise(inv) * 0.5f) * length + to;
+    auto pv3 = normalise(normalise(cross(two, dir)) * sharpness + normalise(inv) * 0.5f) * length + to;
+    auto pv4 = normalise(normalise(cross(two, inv)) * sharpness + normalise(inv) * 0.5f) * length + to;
+
+    vec3 arrow_vertices[] = {
+        from, to, pv1, pv2, pv3, pv4,
+    };
+
+    vec3 arrow_colors[] = {
+        color, color, color, color, color, color,
+    };
+
+    // each number is a vertex, each pair is a line
+    unsigned int arrow_indices[] = {0, 1, 1, 2, 1, 3, 1, 4, 1, 5};
+
+    lines.add(&arrow_vertices[0].v[0], &arrow_colors[0].v[0], 6, &arrow_indices[0], 10);
+}
+
+void Shapes::addGrid(Lines &lines, const vec3 &from, const vec3 &to, const vec3 &color, int divs)
+{
+    int steps = divs + 1;
+
+    vec3 size = to - from;
+    float x = size.v[0] / (steps - 1);
+    float y = size.v[2] / (steps - 1);
+
+    std::vector<vec3> vertices;
+    vertices.resize(steps * steps);
+    std::vector<vec3> colors;
+    colors.resize(steps * steps);
+
+    for (int i = 0; i < steps; ++i)
+    {
+        for (int j = 0; j < steps; ++j)
+        {
+            vertices[j + i * steps] = vec3(from.v[0] + j * x, 0.f, from.v[2] + i * y);
+            colors[j + i * steps] = color;
+        }
+    }
+
+    std::vector<unsigned int> indices;
+    indices.resize(4 * steps);
+    for (int i = 0; i < steps; ++i)
+    {
+        indices[i * 2] = i;
+        indices[i * 2 + 1] = i + steps * (steps - 1);
+    }
+    for (int i = 0; i < steps; ++i)
+    {
+        indices[steps * 2 + i * 2] = i * steps;
+        indices[steps * 2 + i * 2 + 1] = i * steps + (steps - 1);
+    }
+
+    lines.add(&vertices[0].v[0], &colors[0].v[0], vertices.size(), &indices[0], indices.size());
 }
