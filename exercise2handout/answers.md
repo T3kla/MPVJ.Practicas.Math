17c25e084a54aace772ae8979bf05f91195588c2
T3kla <manuillo94@gmail.com>
2021-11-04 11:39:35 +0100

    practica_02 renderizando

---
+++ exercise2handout/answers.md
@@ -0,0 +1,10 @@
+# Práctica 01 Respuestas
+
+He puesto la palabra REVIEW en el código para localizar las zonas donde he modificado código.
+He modificado los archivos `exercise2.h`, `node.cpp`,
+
+```
+asd
+```
+
+asd
--- exercise2handout/node.cpp
+++ exercise2handout/node.cpp
@@ -1,61 +1,87 @@
 #include "Node.h"
 #include "maths_funcs.h"
 
-Node::Node()
-	:parent(0)
-{ ; }
-
-void Node::init() { 
-	position = vec3(0, 0, 0);
-	rotation = versor(0,0,0,1);
-	scale = vec3(1,1,1);
-
-	localMatrix = identity_mat4();
-	worldMatrix = identity_mat4();
-	localInverseMatrix = identity_mat4();
-	worldInverseMatrix = identity_mat4();
-}
-
-void Node::addChild(Node& node) { 
-	node.parent = this;
-	children.push_back(&node);
-}
-
-void Node::removeChild(Node& node) { 
-	auto it = std::find(children.begin(), children.end(), &node);
-	if (it != children.end()) {
-		node.parent = nullptr;
-		children.erase(it);
-	}
-}
-
-void  Node::updateLocal() 
-{ 
-	// todo: given position, rotation and scale, create T,R and S such that
-
-	//localMatrix = T*R*S;
-
-	// todo: given all above, create Sinv, Rinv and Tinv such that
-
-	//localInverseMatrix = Sinv*Rinv*Tinv;
-}
-
-void  Node::updateHierarchy()
+Node::Node() : parent(0)
 {
-	updateLocal();
-	mat4 parentMatrix = identity_mat4();
-	mat4 parentInverseMatrix = identity_mat4();
-	if (parent!= nullptr) {
-		parentMatrix = (*parent).worldMatrix;
-		parentInverseMatrix = (*parent).worldInverseMatrix;
-	}
-	worldMatrix = parentMatrix*localMatrix;
-	worldInverseMatrix = parentInverseMatrix*localInverseMatrix;
-
-	for (size_t i = 0; i < children.size(); ++i) {
-		Node* child = children[i];
-		if (child != nullptr) {
-			(*child).updateHierarchy();
-		}
-	}
 }
+
+void Node::init()
+{
+    position = vec3(0, 0, 0);
+    rotation = versor(0, 0, 0, 1);
+    scale = vec3(1, 1, 1);
+
+    localMatrix = identity_mat4();
+    worldMatrix = identity_mat4();
+    localInverseMatrix = identity_mat4();
+    worldInverseMatrix = identity_mat4();
+}
+
+void Node::addChild(Node &node)
+{
+    node.parent = this;
+    children.push_back(&node);
+}
+
+void Node::removeChild(Node &node)
+{
+    auto it = std::find(children.begin(), children.end(), &node);
+    if (it != children.end())
+    {
+        node.parent = nullptr;
+        children.erase(it);
+    }
+}
+
+// ------------------------- Check file 'answers.md' ------------------------- REVIEW
+void Node::updateLocal()
+{
+    // 00, 04, 08, 12,
+    // 01, 05, 09, 13,
+    // 02, 06, 10, 14,
+    // 03, 07, 11, 15,
+
+    auto T = identity_mat4();
+    auto S = identity_mat4();
+    auto R = quat_to_mat4(rotation);
+
+    T.m[12] = position.x;
+    T.m[13] = position.y;
+    T.m[14] = position.z;
+    T.m[15] = 1.f;
+
+    S.m[0] = scale.x;
+    S.m[5] = scale.y;
+    S.m[10] = scale.z;
+    S.m[15] = 1.f;
+
+    localMatrix = T * R * S;
+    localInverseMatrix = inverse(S) * transpose(R) * inverse(T);
+}
+
+void Node::updateHierarchy()
+{
+    updateLocal();
+
+    // Set parent matrices as if they were 0,0
+    mat4 parentMatrix = identity_mat4();
+    mat4 parentInverseMatrix = identity_mat4();
+
+    // Search parent matrices
+    if (parent != nullptr)
+    {
+        parentMatrix = parent->worldMatrix;
+        parentInverseMatrix = parent->worldInverseMatrix;
+    }
+
+    // Localize node space in parent space
+    // worldMatrix is the addition of all node spaces up until this node
+    worldMatrix = parentMatrix * localMatrix;
+    worldInverseMatrix = parentInverseMatrix * localInverseMatrix;
+
+    // Update children
+    for (auto &child : children)
+        if (child != nullptr)
+            child->updateHierarchy();
+}
+// ---------------------------------------------------------------------------
